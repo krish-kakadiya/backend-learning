cad611fabcb4ceb5cca33b85cea814959166103d
krish kakadiya <krishkakadiya000007@gmail.com>
2025-05-16 14:15:02 +0530

    connect to the DB

---
+++ src/index.js
@@ -0,0 +1,42 @@
+// require('dotenv').config()
+import dotenv from "dotenv"
+import mongoose  from "mongoose";
+import { DB_NAME } from "./constants.js";
+//when we try to connect whith data base we shoul use try catch cause we have vary high chance of error
+import express from "express"
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB();
+
+
+
+
+
+
+
+
+
+/*
+(async () =>{
+    try{
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error",(error)=>{
+            console.log("error:",error);
+            throw error
+        })
+
+        app.listen(process.env.PORT,()=>{
+            console.log('app is listening on port:',process.env.PORT);
+            
+        })
+
+    }catch(error){
+        console.log('ERROR:' ,error)
+        throw error;
+    }
+})()
+    */
