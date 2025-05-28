461094528772c1db8f251d1f374cf0125fd783af
krish-kakadiya <krishkakadiya000007@gmail.com>
2025-05-28 17:21:41 +0530

    add register user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,10 +2,21 @@ import { asyncHandler } from '../utils/asyncHandler.js';
 
 
 const registerUser = asyncHandler(async (req, res)=>{
-    res.status(200).json({
-        success: true,
+    //step for user registration
+    //1. get user data from request body
+    //2. validate user data
+    //3. check if user already exists
+    // Here you would typically check the database to see if the user already exists
+    // For demonstration, let's assume the user does not exist
+    //4. create new user
+    //5. save user to database
+    // Here you would typically save the user to the database
+    // For demonstration, let's assume the user is saved successfully
+    //6. send response
+    const {fullname, email, username, password} = req.body
+    console.log(email)
+    res.status(201).json({
         message: 'User registered successfully',
-        user: req.user // Assuming user data is set in the request
     });
 })
 
