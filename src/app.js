b1f6c7e4cb3cfe7f261b056e6c54b0d1ba8f9482
krish-kakadiya <krishkakadiya000007@gmail.com>
2025-05-28 16:02:13 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -10,14 +10,14 @@ app.use(cors({
 }))
 
 app.use(express.json({limit:"16kb"}))
-
 app.use(express.urlencoded({extended:true, limit:'16kb'}))
-
 app.use(express.static('public')) 
-
 app.use(cookieParser())
 
-console.log(typeof app)
+import userRouter from "./routes/user.routes.js";
+app.use("/api/v1/users",userRouter);
+
+// https://localhost:8000/api/v1/users/register
 
 export { app } 
 
---
+++ src/controllers/user.controller.js
@@ -0,0 +1,12 @@
+import { asyncHandler } from '../utils/asyncHandler.js';
+
+
+const registerUser = asyncHandler(async (req, res)=>{
+    res.status(200).json({
+        success: true,
+        message: 'User registered successfully',
+        user: req.user // Assuming user data is set in the request
+    });
+})
+
+export { registerUser };
