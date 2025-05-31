8061dd03d8f436a22aef16d3fc0ad7ffa22922c1
krish-kakadiya <krishkakadiya000007@gmail.com>
2025-05-31 18:29:21 +0530

    update user routes to secure change password, current user, and watch history endpoints with JWT verification

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,8 @@
 import { Router } from 'express';
-import { loginuser, logoutUser, registerUser, refreshAccessToken, changePassword} from '../controllers/user.controller.js';
+import { loginuser, logoutUser, registerUser, refreshAccessToken, changePassword, getCurrentUser, watchHistory, getUserCHannelProfile} from '../controllers/user.controller.js';
 import { upload } from '../middlewares/multer.middleware.js';
 import { verifyJWT } from '../middlewares/auth.middleware.js';
+import { get } from 'mongoose';
 
 const router = Router();
 
@@ -24,6 +25,8 @@ router.route("/login").post(loginuser)
 // secured routes
 router.route("/logout").post(verifyJWT,logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
-router.route("/change-password").put(changePassword);
-
+router.route("/change-password").put(verifyJWT,changePassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/c/:username").get(verifyJWT, getUserCHannelProfile);
+router.route("/watch-history").get(verifyJWT, watchHistory);
 export default router;
