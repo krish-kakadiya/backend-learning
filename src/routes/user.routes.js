d26b601a850f787fc0be67618bbe85ff8acf6e23
krish-kakadiya <krishkakadiya000007@gmail.com>
2025-05-31 10:33:42 +0530

    correct rout of logout

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -22,6 +22,6 @@ router.route("/register").post(
 router.route("/login").post(loginuser)
 
 // secured routes
-router.route("logout").post(verifyJWT,logoutUser);
+router.route("/logout").post(verifyJWT,logoutUser);
 
 export default router;
